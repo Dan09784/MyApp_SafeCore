78e084dc2356e964fb61dd2e2a688a306d9b5684
Dan09784 <Dan09784@icloud.com>
2025-08-18 10:09:18 +0100

    evolution de mon apss

--- safecore/lib/main.dart
+++ safecore/lib/main.dart
@@ -2,22 +2,24 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 void main() {
-  runApp(const SafeCoreApp());
+  runApp(const SafeCoreAdminApp());
 }
 
-class SafeCoreApp extends StatelessWidget {
-  const SafeCoreApp({Key? key}) : super(key: key);
+/// Widget racine
+class SafeCoreAdminApp extends StatelessWidget {
+  const SafeCoreAdminApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "SafeCore Admin",
+      title: 'SafeCore Admin',
       debugShowCheckedModeBanner: false,
-      theme:ThemeData(
+      theme: ThemeData(
+        // thème principal : clair + accent orange
         primarySwatch: Colors.deepOrange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home : const DashboardPage(),
+      home: const  SafeCoreAdminApp(),
     );
   }
 }
