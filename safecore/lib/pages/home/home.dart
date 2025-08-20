02dd3eae2c01021dd077526c1d89e423094fdadf
Dan09784 <Dan09784@icloud.com>
2025-08-20 18:37:22 +0100

    evolution de l'aprentissage

--- safecore/lib/pages/home/home.dart
+++ safecore/lib/pages/home/home.dart
@@ -6,14 +6,34 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 88, 37, 229),
+      backgroundColor: const Color.fromARGB(255, 250, 250, 251),
       body: SingleChildScrollView(
         child: Stack(
-          children: [
-            Image.asset(
-            'assets/images/logoo_mon.jpg' ),
-          ],
-      )),
+          children: [Image.asset('assets/images/logoo_mon.jpg', width: 254)],
+        ),
+      ),
+      bottomNavigationBar: NavigationBar(),
+    );
+  }
+
+  Widget NavigationBar() {
+    return BottomNavigationBaritems(items: (
+    BottomNavigationBarItem(
+      label: "home",
+      icon: Icon(Icons.home)
+    ),
+    BottomNavigationBarItem(
+      label: "home",
+      icon: Icon(Icons.home)
+    ),
+    BottomNavigationBarItem(
+      label: "home",
+      icon: Icon(Icons.home)
+    ),
+    BottomNavigationBarItem(
+      label: "home",
+      icon: Icon(Icons.home)
+    ),
     );
   }
 }
